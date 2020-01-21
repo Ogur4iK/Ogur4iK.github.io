50dda9af4aa577405d51eb0dd24e8c27e14371d7
Ogur4iK <koryavi2014@mail.ru>
2020-01-22 00:58:32 +0300

    dff

---
+++ stk/js/script.js
@@ -0,0 +1,45 @@
+jQuery(function($){
+    $('.slider').owlCarousel({
+        loop: true,
+        items: 1,
+        nav: true,
+        autoplay: true,
+        autoplayHoverPause: true,
+        navText: ['<svg class="slider-icon"><use xlink:href="#arrow-right"></use></svg>',
+        '<svg class="slider-icon"><use xlink:href="#arrow-left"></use></svg>']
+    });
+
+    $('#feedback-modal').iziModal({
+        overlayColor: 'rgba(0,0,0,0.7)',
+        borderBottom: false,
+        radius: 5,
+        width: 400,
+        history: true
+    });
+
+    $('#services-modal').iziModal({
+        overlayColor: 'rgba(0,0,0,0.7)',
+        borderBottom: false,
+        radius: 5,
+        width: 800,
+        history: true
+    });
+
+    $('.feedback-open').click(function(){
+        $('#feedback-modal').iziModal('open');
+    });
+
+    $('.buy-open').click(function(){
+        $(this).parents('[id*="modal"]').iziModal('close');
+        $('#feedback-modal').iziModal('open');
+    });
+
+    $('.more-link').click(function(){
+        event.preventDefault();
+        $('#services-modal').iziModal('open');
+    });
+
+    $(".close-icon").click(function(){
+        $(this).parents('[id*="modal"]').iziModal('close');
+    });
+});
